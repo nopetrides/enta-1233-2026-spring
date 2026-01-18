14c64f21762e94d95758cc89deab74eb99296ac4
nopetrides <nopetrides@gmail.com>
2026-01-17 16:11:43 -0800

    Animations for slides

--- Assets/_1233_StudentWork/Scripts/Player/PlayerController.cs
+++ Assets/_1233_StudentWork/Scripts/Player/PlayerController.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+	// Avoid using GetComponent
 	[SerializeField] private CharacterController _characterController;
 	[SerializeField] private float _speed = 5f;
 	[SerializeField] private float _smoothTime = 0.05f;
 	[SerializeField] private float _gravityMultiplier = 3f;
-	
+	[SerializeField] private float _jumpStrength = 5f;
+	[SerializeField] private int _maxNumberOfJumps = 3;
 	
 	[Header("Debug Serialization")]
 	[SerializeField] // debug
@@ -23,6 +24,10 @@ public class PlayerController : MonoBehaviour
 	private float _gravity = -9.81f;
 	[SerializeField]
 	private float _gravityVelocity;
+	[SerializeField]
+	private bool _isGrounded;
+	[SerializeField]
+	private int _numberOfJumps;
 	
 	/// <summary>
 	/// Axis (WASD or left stick) -1 to +1 for each axis
@@ -36,7 +41,11 @@ public class PlayerController : MonoBehaviour
 
 	public void OnJump(InputAction.CallbackContext context)
 	{
-		_jump = context.ReadValueAsButton();
+		if (!context.started) return;
+		if (!_isGrounded && _numberOfJumps >= _maxNumberOfJumps) return;
+		_numberOfJumps++;
+		_gravityVelocity = _jumpStrength;
+		_isGrounded = false;
 	}
 
 	/// <summary>
@@ -45,13 +54,15 @@ public class PlayerController : MonoBehaviour
 	public void Update()
 	{
 		Gravity();
-		if (_moveInput.sqrMagnitude < 0.01f) return;
 		Rotate();
 		Move();
+		AnimationParameters();
 	}
 
 	private void Rotate()
 	{
+		// Prevents character from rotating when no input
+		if (_moveInput.sqrMagnitude < 0.01f) return;
 		var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
 		var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, _smoothTime);
 		transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -64,11 +75,44 @@ public class PlayerController : MonoBehaviour
 
 	private void Gravity()
 	{
-		if (_characterController.isGrounded) 
+		if (_isGrounded) 
 			_gravityVelocity = -1f;
 		else
 			_gravityVelocity += _gravity * _gravityMultiplier * Time.deltaTime;
 		
 		_direction.y = _gravityVelocity;
 	}
+
+	private void GroundCheck()
+	{
+		// Specific ground check so we only check once per frame
+		_isGrounded = _characterController.isGrounded;
+		if (_isGrounded) _numberOfJumps = 0;
+	}
+
+	/// <summary>
+	/// Not using the coroutine method from the example.
+	/// I prefer to know exactly when the grounded check is.
+	/// </summary>
+	/// <returns></returns>
+	private System.Collections.IEnumerator WaitForLanding()
+	{
+		yield return new WaitUntil(() => !_isGrounded);
+		yield return new WaitUntil(() => _characterController.isGrounded);
+		
+		_numberOfJumps = 0;
+	}
+	
+	[SerializeField] 
+	private Animator _animator;
+	
+	private static readonly int Speed = 
+		Animator.StringToHash("Speed");
+	
+	private void AnimationParameters()
+	{
+		_animator.SetFloat(
+			Speed, _moveInput.sqrMagnitude);
+	}
+	
 }
